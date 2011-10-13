ca000bbb63f2635ffc399aa7a7c955f6a6008bad
therouv <therouv@googlemail.com>
2011-10-13 23:50:26 +0200

    [~TASK] Added files in new structure

---
+++ src/app/code/community/FireGento/Core/Model/Log/Firelogger.php
@@ -0,0 +1,54 @@
+<?php
+/**                                                                       
+ * This file is part of the FIREGENTO project.
+ * 
+ * FireGento_Core is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License version 3 as 
+ * published by the Free Software Foundation.
+ * 
+ * This script is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * PHP version 5
+ *
+ * @category  FireGento
+ * @package   FireGento_Core
+ * @author    FireGento Team <team@firegento.com>
+ * @copyright 2011 FireGento Team (http://www.firegento.de). All rights served.
+ * @license   http://opensource.org/licenses/gpl-3.0 GNU General Public License, version 3 (GPLv3)
+ * @version   $$Id$$
+ */
+require_once 'FireGento/FireLogger/FireLogger.php';
+
+if (!defined('FIRELOGGER_NO_CONFLICT')) define('FIRELOGGER_NO_CONFLICT', true);
+if (!defined('FIRELOGGER_NO_EXCEPTION_HANDLER')) define('FIRELOGGER_NO_EXCEPTION_HANDLER', true);
+if (!defined('FIRELOGGER_NO_ERROR_HANDLER')) define('FIRELOGGER_NO_ERROR_HANDLER', true);
+//if (!defined('FIRELOGGER_NO_OUTPUT_HANDLER')) define('FIRELOGGER_NO_OUTPUT_HANDLER', true);
+if (!defined('FIRELOGGER_NO_DEFAULT_LOGGER')) define('FIRELOGGER_NO_DEFAULT_LOGGER', true);
+/**
+ * FireLogger Class
+ *
+ * @category  FireGento
+ * @package   FireGento_Core
+ * @author    FireGento Team <team@firegento.com>
+ * @copyright 2011 FireGento Team (http://www.firegento.de). All rights served.
+ * @license   http://opensource.org/licenses/gpl-3.0 GNU General Public License, version 3 (GPLv3)
+ * @version   $$Id$$
+ */
+class FireGento_Core_Model_Log_Firelogger extends FireLogger
+{
+    /**
+     * Class Constructor
+     * 
+     * @return void
+     */
+    public function __construct()
+    {
+        $flag = Mage::helper('firegento/log')->isFireloggerAllowed();
+        FireLogger::$enabled = $flag;
+        if ($flag) {
+            parent::__construct('php', 'background-color: #9998d1');
+        }
+    }
+}
